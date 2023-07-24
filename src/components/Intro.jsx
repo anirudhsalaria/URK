1cb57151285a6a191dd00becead21e62d4477084
Anirudh Salaria <salariaanirudh@gmail.com>
2023-07-24 20:43:44 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className='w-[300px] border h-[100vh] bg-jacarta-100'>
-      Intro
+      <div>
+        <span>Today</span>
+        <div className='border'>
+          <div className='font-bold text-xl'>$19892</div>
+          <div className='font-semibold text-jacarta-500 text-[10px]'>Account Balance</div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   )
 }
